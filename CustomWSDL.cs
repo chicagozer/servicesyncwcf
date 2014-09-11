96197b6fb4368624574830404eb33bfa71d05c7e
Jim Mittler <jim.mittler@xtime.com>
2014-09-11 15:55:43 -0700

    load WSDL class

---
+++ CustomWSDL.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+[ServiceBehavior(InstanceContextMode = InstanceContextMode.PerCall)]
+public class CustomWsdlService : ICustomWsdl
+{
+    private string renameXsd(string src)
+    {
+        XmlDocument xdoc = new XmlDocument();
+        xdoc.LoadXml(src);
+        XmlNodeList xmlnodes = xdoc.GetElementsByTagName("xsd:include");
+        updateNodes(xmlnodes);
+        xmlnodes = xdoc.GetElementsByTagName("xsd:import");
+        updateNodes(xmlnodes);
+        return xdoc.InnerXml;
+    }
+
+    private void updateNodes(XmlNodeList xmlnodes)
+    {
+        for (int i = 0; i < xmlnodes.Count; i++)
+        {
+            string xsdName = xmlnodes[i].Attributes["schemaLocation"].Value as string;
+            if (!string.IsNullOrEmpty(xsdName))
+            {
+                string newName = "MetaData?name="
+                  + Path.GetFileNameWithoutExtension(xsdName) + "&extension=xsd";
+                xmlnodes[i].Attributes["schemaLocation"].Value = newName;
+            }
+        }
+    }
+
+    public Stream MetaData(string name, string extension)
+    {
+        string path =
+           Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+        string src =
+           renameXsd(File.ReadAllText(path + "\\" + name + "." + extension));
+        return new System.IO.MemoryStream(System.Text.Encoding.UTF8.GetBytes(src));
+    }
+}
---
+++ ICustomWSDL.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Xtime.ServiceSync
+{
+    [ServiceContract]
+    public interface ICustomWsdl
+    {
+        [OperationContract]
+        [WebGet()]
+        Stream MetaData(string name, string extension);
+    }
+}
